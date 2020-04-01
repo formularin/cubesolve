2eab3e832c7dbae986c74aaea14aa178f00de631
lol-cubes <arinmkhare@gmail.com>
2020-04-01 14:18:25 -0400

    add more maps for move constructor

--- include/moves.hpp
+++ include/moves.hpp
@@ -19,13 +19,31 @@ using std::vector;
 
 // Index of row or col of 3x3 face vector that contains stickers that
 // would change as a result of each move.
-static std::map<std::string, int> move_coords = {
-    { "L", 2 },
-    { "R", 0 },
-    { "D", 2 },
-    { "U", 0 },
-    { "B", 2 },
-    { "F", 0 }
+static std::map<char, int> move_coords = {
+    { 'L', 2 },
+    { 'R', 0 },
+    { 'D', 2 },
+    { 'U', 0 },
+    { 'B', 2 },
+    { 'F', 0 }
+};
+static std::map<char, int> move_axes = {
+    { 'L', 1 },
+    { 'R', 1 },
+    { 'D', 0 },
+    { 'U', 0 },
+    { 'B', 2 },
+    { 'F', 2 }
+};
+// What direction attribute a move object should have for each face
+// being moved if move is not prime.
+static std::map<char, std::string> move_directions = {
+    { 'L', "CC" },
+    { 'R', "C" },
+    { 'D', "CC" },
+    { 'U' , "C" },
+    { 'B', "CC" },
+    { 'F', "C" }
 };
 
 
