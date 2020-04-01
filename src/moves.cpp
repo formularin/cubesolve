26e701aead0e88883dfa9f1f7f450bad3faddea4
lol-cubes <arinmkhare@gmail.com>
2020-04-01 14:53:29 -0400

    write Move class constructor

--- src/moves.cpp
+++ src/moves.cpp
@@ -7,4 +7,38 @@
 ========================================*/
 
 
+#include <iostream>
+#include <string>
+
 #include "../include/moves.hpp"
+
+
+Move::Move(std::string move_string) {
+    // Move class constructor.
+
+    move = move_string;
+
+    char last_char = move_string.at(move_string.size() - 1);
+    char first_char = move_string.at(0);
+
+    if ( last_char == '2' ) {
+        rotations = 2;
+    } else {
+        rotations = 1;
+    }
+
+    int axis = move_axes[first_char];
+    int coords = move_coords[first_char];
+
+    if ( last_char != '\'' ) {
+        // Move is prime
+        direction = move_directions[first_char];
+    } else {
+        if ( move_directions[first_char] == "C" ) {
+            direction = "CC";
+        } else {
+            direction = "C";
+        }
+    }
+
+}
