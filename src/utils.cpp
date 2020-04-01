3fd6dbaac6d5540dcdc5a88329f120804abc2247
lol-cubes <arinmkhare@gmail.com>
2020-04-01 13:47:46 -0400

    fix typo in utils.cpp that was causing stackoverflow

--- src/utils.cpp
+++ src/utils.cpp
@@ -22,7 +22,7 @@ bool get_int_in_vector(int i, vector<int> vec) {
 
 void print_2d_vector(vector< vector<int> > vec) {
     for ( int x = 0; x < vec.size(); x++ ) {
-        for ( int y = 0; x < vec.back().size(); y++ ) {
+        for ( int y = 0; y < vec.back().size(); y++ ) {
             std::cout << vec[x][y] << " ";
         }
         std::cout << std::endl;
